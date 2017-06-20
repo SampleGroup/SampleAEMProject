406725dd442b2090c56323c5f64d904150ced233
manik <manik@LAPTOP-RMEIUEJ7>
2017-06-20 20:25:15 +0530

    asd

--- core/src/main/java/com/myproject/services/core/servlets/CommonSlingServlet.java
+++ core/src/main/java/com/myproject/services/core/servlets/CommonSlingServlet.java
@@ -23,7 +23,7 @@ public class CommonSlingServlet extends SlingAllMethodsServlet {
 
 	/**
 	 * *********sdfghjkl,mnbxcvbnm,.,mfdcvbnm,
-	 *sxdfghjkl;'';lkmjasdfghjklkjhfdsdfbhnjmvggg
+	 *sxdfghjkl;'';lkmjasdfghjklkjhfdsdfbhnjmvgggkikikkikikikikikiki
 	//private static final long serialVersionUID = 1L;
 	/**
 	 * 
